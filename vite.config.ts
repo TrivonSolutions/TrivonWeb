979e038664321856685873355a3bcbb23ee62ea6
shaquille reichstein <ceo@trivon.dk>
2025-10-07 19:20:08 +0200

    Virker det?

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const repoName = "TrivonWeb";
-
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? `/${repoName}/` : "/", 
+export default defineConfig({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), componentTagger()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
